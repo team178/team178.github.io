3bc1db44d861af1d1e25416c5cfbd02bec5e9b62
Giselle Koo <gisellegk@gmail.com>
2015-11-05 20:02:15 -0500

    Built raffle meter display for website.

---
+++ _includes/raffle-meter/raffle-meter.js
@@ -0,0 +1,36 @@
+google.load('visualization', 1.0);
+      google.setOnLoadCallback(test);
+      function test() {
+        var opts = {sendMethod: 'auto'};
+        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1X8Ba3bd-JEhASUNs5VDcA18dTPyxIS5lwxRKBBKWKqs/edit#gid=0', opts);
+        query.setQuery('select F');
+        query.send(handleQueryResponse);
+      }
+      function handleQueryResponse(response){
+        if(response.isError()){
+          console.log('Error: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+          return;
+        }
+
+        var data = response.getDataTable();
+        var red = 0;
+        var green = 0;
+        var purple = 0;
+
+        for(var i = 0; i < data.getNumberOfRows(); i++){
+          if(data.getValue(i,0) == 0) {
+            red += 1;
+          } else if(data.getValue(i,0) == 1) {
+            purple += 1;
+          } else if(data.getValue(i,0) == 2) {
+            green += 1;
+          }
+        }        
+        var redPercent = 100;
+        var greenPercent = green / (green + purple + red) * 100;
+        var purplePercent = (purple + green) / (green + purple + red) * 100;
+
+        document.getElementById('percent').innerHTML = "Percent of tickets sold: " + greenPercent + "%";
+        document.styleSheets[0].insertRule('.halfStyle.hs-vertical-third:before { width:' + greenPercent + '%; }', 0);
+        document.styleSheets[0].insertRule('.halfStyle.hs-vertical-third:after { width:' + purplePercent + '%; }', 0);
+      }
