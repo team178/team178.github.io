d56f412f86e6ab3c1cac41f0c38997d639f7440f
TJScalzo <timtv25@gmail.com>
2015-11-19 19:36:03 -0500

    We fixed the way that the widths are set as well as cross-browser compatability. Fixes #35

--- assets/js/raffle-meter.js
+++ assets/js/raffle-meter.js
@@ -1,37 +1,41 @@
 google.load('visualization', 1.0);
-      google.setOnLoadCallback(test);
-      function test() {
-        var opts = {sendMethod: 'auto'};
-        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1X8Ba3bd-JEhASUNs5VDcA18dTPyxIS5lwxRKBBKWKqs/edit#gid=0', opts);
-        query.setQuery('select F');
-        query.send(handleQueryResponse);
-      }
-      function handleQueryResponse(response){
-        if(response.isError()){
-          console.log('Error: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-          return;
-        }
+google.setOnLoadCallback(test);
+function test() {
+  var opts = {sendMethod: 'auto'};
+  var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1X8Ba3bd-JEhASUNs5VDcA18dTPyxIS5lwxRKBBKWKqs/edit#gid=0', opts);
+  query.setQuery('select F');
+  query.send(handleQueryResponse);
+}
+function handleQueryResponse(response){
+  if(response.isError()){
+    console.log('Error: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+  var data = response.getDataTable();
+  var base = 0;
+  var sold = 0;
+  var out = 0;
+  for(var i = 0; i < data.getNumberOfRows(); i++){
+    if(data.getValue(i,0) == 0) {
+      base += 1;
+    } else if(data.getValue(i,0) == 1) {
+       out += 1;
+    } else if(data.getValue(i,0) == 2) {
+       sold += 1;
+    }
+  }        
 
-        var data = response.getDataTable();
-        var red = 0;
-        var green = 0;
-        var purple = 0;
+  var soldPercent = sold / (sold + out + base) * 100;
+  var outPercent = (out + sold) / (sold + out + base) * 100;
 
-        for(var i = 0; i < data.getNumberOfRows(); i++){
-          if(data.getValue(i,0) == 0) {
-            red += 1;
-          } else if(data.getValue(i,0) == 1) {
-            purple += 1;
-          } else if(data.getValue(i,0) == 2) {
-            green += 1;
-          }
-        }        
-        var redPercent = 100;
-        var greenPercent = green / (green + purple + red) * 100;
-        var purplePercent = (purple + green) / (green + purple + red) * 100;
+  document.getElementById('percent').innerHTML = "Percent of tickets sold: " + soldPercent + "%";
 
-        document.getElementById('percent').innerHTML = "Percent of tickets sold: " + greenPercent + "%";
-        console.log(document.styleSheets);
-        document.styleSheets[7].insertRule('.halfStyle.hs-vertical-third:before { width:' + greenPercent + '%; }', 0);
-        document.styleSheets[7].insertRule('.halfStyle.hs-vertical-third:after { width:' + purplePercent + '%; }', 0);
-      }
+  var sheet = new StyleSheet();
+  var widthSold = new StyleSheetElement("width", soldPercent + '%');
+  var tagA = sheet.addElementToTag(".halfStyle.hs-vertical-third:before", widthSold);
+  var widthOut = new StyleSheetElement("width", outPercent + '%');
+  var tagHover = sheet.addElementToTag(".halfStyle.hs-vertical-third:after", widthOut);
+  console.log(sheet);
+  addInlineStyleSheet(sheet);
+
+}
