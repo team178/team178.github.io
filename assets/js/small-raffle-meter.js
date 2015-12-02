c020653d8aaf66464ca042778c5b9a6f460fa1f1
TJScalzo <timtv25@gmail.com>
2015-12-02 15:26:11 -0500

    Did comment stuff. Fixed height of percent on mobile. #60

--- assets/js/small-raffle-meter.js
+++ assets/js/small-raffle-meter.js
@@ -27,13 +27,14 @@ function handleQueryResponse(response){
 
   var soldPercent = sold / (sold + out + base) * 100;
   var outPercent = (out + sold) / (sold + out + base) * 100;
-
   
+  // For tooltip
   var deadline = new Date(2015, 11, 10, 18, 0, 0); // note: month is 0-11 but date starts at 1
   var numDays = Math.ceil((deadline - new Date())/86400000);
-
+  
   $("#percent").html("Tickets sold: " + soldPercent + "% | " + numDays + " days left! | <a href='/raffle'>About the raffle >></a>");
-
+  
+  // For meter again
   var sheet = new StyleSheet();
   var widthSold = new StyleSheetElement("width", soldPercent + '%');
   var tagA = sheet.addElementToTag("#raffle-meter-sold", widthSold);
