dbd014e2629481800727af243cd9e54aef01c596
Tim Scalzo <timtv25@gmail.com>
2015-12-04 11:34:02 -0500

    Moved tooltip and attempted #62

--- assets/js/raffle-meter.js
+++ assets/js/raffle-meter.js
@@ -28,13 +28,6 @@ function handleQueryResponse(response){
   var soldPercent = sold / (sold + out + base) * 100;
   var outPercent = (out + sold) / (sold + out + base) * 100;
   
-  // For tooltip
-  var deadline = new Date(2015, 11, 10, 18, 0, 0); // note: month is 0-11 but date starts at 1
-  var numDays = Math.ceil((deadline - new Date())/86400000);
-  
-  $("#percent").html("Tickets sold: " + soldPercent + "% | " + numDays + " days left! | <a href='/raffle'>About the raffle >></a>");
-  
-  // For meter again
   var sheet = new StyleSheet();
   var widthSold = new StyleSheetElement("width", soldPercent + '%');
   var tagA = sheet.addElementToTag(".halfStyle.hs-vertical-third:before", widthSold);
@@ -42,5 +35,11 @@ function handleQueryResponse(response){
   var tagB = sheet.addElementToTag(".halfStyle.hs-vertical-third:after", widthOut);
   console.log(sheet);
   addInlineStyleSheet(sheet);
+  
+   // For tooltip
+  var deadline = new Date(2015, 11, 10, 18, 0, 0); // note: month is 0-11 but date starts at 1
+  var numDays = Math.ceil((deadline - new Date())/86400000);
+  var soldPercent = soldPercent.substring(0,5); // makes length of sold percentage 5 characters long
+  $("#percent").html("Tickets sold: " + soldPercent + "% | " + numDays + " days left! | <a href='/raffle'>About the raffle >></a>");
   $("#raffle-meter-container").show(1000);
 }
