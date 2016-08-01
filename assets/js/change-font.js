006682489abdf6c24a7f55b34ee9a090dd4dfeae
TJScalzo <timtv25@gmail.com>
2016-08-01 18:08:26 -0400

    Puntuation in popups and change font within popups. #113

--- assets/js/change-font.js
+++ assets/js/change-font.js
@@ -10,9 +10,11 @@ function fontCookieCheck() {
 }
 
 function fontAlert1() {
+  $(".swal2-modal").css('fontFamily', 'Open Dyslexic');
+  $("button").css('fontFamily', 'Open Dyslexic');
   swal({
     title: 'Dyslexia Font',
-    text: 'To use the dyslexia friendly font <i>Open Dyslexic</i> on this site click, the button below.',
+    text: 'To use the dyslexia friendly font <i>Open Dyslexic</i> on this site, select the button below.',
     type: 'info',
     showCancelButton: true,
     confirmButtonText: 'Open Dyslexic',
@@ -23,7 +25,7 @@ function fontAlert1() {
       createFontCookie("Open Dyslexic");
       swal({
         title: 'You\'ve set the font!',
-        text: 'To change the font temporarily, toggle the switch at the bottom of the page.<br><br>To change your settings back to the original font, click the button next to the switch.',
+        text: 'To change the font temporarily, toggle the switch at the bottom of the page.<br><br>To change your settings back to the original font, select the button next to the switch.',
         showCloseButton: true,
         type: 'success',
       });
@@ -31,7 +33,7 @@ function fontAlert1() {
       createFontCookie("Open Sans");
       swal({
         title: 'You\'ve kept the same font!',
-        text: 'To change the font temporarily, toggle the switch at the bottom of the page.<br><br>To change your settings to use <i>Open Dyslexic</i>, click the button next to the switch.',
+        text: 'To change the font temporarily, toggle the switch at the bottom of the page.<br><br>To change your settings to use <i>Open Dyslexic</i>, select the button next to the switch.',
         showCloseButton: true,
         type: 'success',
       });
@@ -59,6 +61,8 @@ function toggleFont() {
 }
 
 function fontAlert2() {
+  $(".swal2-modal").css('fontFamily', 'Open Dyslexic');
+  $("button").css('fontFamily', 'Open Dyslexic');
   swal({
     title: 'Which font?',
     type: 'question',
@@ -87,7 +91,7 @@ function fontAlert2() {
     } else {
       swal(
         'Nothing\'s changed',
-        'To change the font temporarily, toggle the switch at the bottom of the page.<br><br>To change your settings to use a different font, click the button next to the switch.'
+        'To change the font temporarily, toggle the switch at the bottom of the page.<br><br>To change your settings to use a different font, select the button next to the switch.'
       );
     }
   });
@@ -95,7 +99,9 @@ function fontAlert2() {
 
 function setFont(font) {
   if (font != null) {
-    document.body.style.fontFamily = font;
+    $("body").css('fontFamily', font);
+    $(".swal2-modal").css('fontFamily', font);
+    $("button").css('fontFamily', font);
     // This part makes everything zoom out to look like it's the right size.
     // TODO: Make things look *more* correct by setting other styles to be different.
     if ($("body").css('fontFamily') == "'Open Dyslexic'") {
