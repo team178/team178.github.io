59031af90918c09349acb3e302f31525642c223c
TJScalzo <timtv25@gmail.com>
2016-09-18 15:30:57 -0400

    Removed last year's game image and modified flags points of disappearance.

--- assets/js/flag.js
+++ assets/js/flag.js
@@ -1,9 +1,9 @@
 function toggleFlag() {
-  if ($( window ).width() <= 950) { /* Checks if window width is below 950 pixels. */
+  if ($( window ).width() <= 780) { /* Checks if window width is below 780 pixels. */
     document.getElementsByClassName("flags")[1].style.display = "none";
 /* Looks for all elements whose class is "flags" and makes the second one detected not visible. */
   }
-  else { /* Does the below if window width isn't below 950 pixels. */
+  else { /* Does the below if window width isn't below 780 pixels. */
     document.getElementsByClassName("flags")[1].style.display = "block";
 /* Looks for all elements whose class is "flags" and makes the second one detected visible. */
   }
