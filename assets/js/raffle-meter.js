df448bd11c1f9e031ba5ded2d9d57d9e12c8032f
TJScalzo <timtv25@gmail.com>
2016-11-03 22:44:43 -0400

    Updated raffle page completely. Raffle meter needs work still

--- assets/js/raffle-meter.js
+++ assets/js/raffle-meter.js
@@ -37,9 +37,9 @@ Thank you for taking the time to read this.
 ~~~~~~-Tim Scalzo
 */
 google.load('visualization', 1.0);
-// Note: The month is 0-11 but date starts at 1
-var raffle_deadline = new Date(2015, 11, 10, 20, 15, 0); // Make sure to set this to the proper deadline.
-var numDays = Math.ceil((raffle_deadline - new Date())/3600000);
+// For info on how `Date()` is formatted, check out http://www.w3schools.com/jsref/jsref_obj_date.asp
+var raffle_deadline = new Date(2016, 11, 13, 19, 30, 0); // Make sure to set this to the proper deadline.
+var numDays = Math.ceil((raffle_deadline - new Date())/86400000);
 
 var container; // entire progress bar
 var soldTag; // blue
@@ -62,7 +62,7 @@ $( document ).ready(function() {
     google.setOnLoadCallback(get_data);
     function get_data() {
       var opts = {sendMethod: 'auto'};                // Make sure to manually change the link to the proper Google Sheet.
-      var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1X8Ba3bd-JEhASUNs5VDcA18dTPyxIS5lwxRKBBKWKqs/edit#gid=0', opts);
+      var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1xEjQEKM2FLlj_dGCjKW4JWWDM-gDN0r2YXIFon_zraM/edit#gid=0', opts);
       query.setQuery('select F'); // Make sure to manually select the proper column.
       query.send(handleQueryResponse);
       // Note: The function will only run when the page is loaded.
@@ -83,7 +83,7 @@ $( document ).ready(function() {
       var sold = 0;
       var out = 0;
       for(var i = 0; i < data.getNumberOfRows(); i++){ // The i++ adds 1 to 'i' after it goes through the loop once.
-        if(data.getValue(i,0) == 0) { // Checks for 0 in row 'i'.
+        if(data.getValue(i,0) == 0 || data.getValue(i,0) == 9) { // Checks for 0 and 9 in row 'i'.
           base += 1; // Adds 1 to the base if ^^^^ is true.
         } else if(data.getValue(i,0) == 1) { // Checks for a 1 in row 'i'.
           out += 1;
@@ -113,15 +113,15 @@ $( document ).ready(function() {
 
       var timeLeft; // Generates the phrase that is the time left.
       if(numDays > 1) { // in days
-        var timeLeft = numDays + "days left"
+        var timeLeft = numDays + " days left"
       } else if(numDays <= 1 && numDays > (1/24)) { // in hours
-        var timeLeft = numDays / 24 + "hours left"
+        var timeLeft = numDays / 24 + " hours left"
       } else if(numDays <= (1/24) && numDays > 0) { // in minutes
-        var timeLeft = "Only" + numDays / 1440 + "minutes left!"
+        var timeLeft = "Only " + numDays / 1440 + " minutes left!"
       } else{ // no time left
         var timeLeft = "There's no time left!"
       }
-      $("#percent").html("Tickets sold: " + soldPercentShort + "% | $" + money + "+ earned | "+ timeLeft +"<div class='raffle-link'> | <a href='/raffle'>About the raffle >></a></div>");
+      $("#percent").html("Tickets sold: " + soldPercentShort + "% | $" + money + "+ earned | "+ timeLeft +"<span class='raffle-link'> | <a href='/raffle'>About the raffle >></a></span>");
     }
 
   // This will run if you change the true to a false; it'll also change if the days after the raffle exceeds two.
@@ -136,7 +136,7 @@ $( document ).ready(function() {
     var tagB = sheet.addElementToTag(".tooltip:hover:after", tooltipWidth); // Injects tootltipWidth into the tooltip's class.
     addInlineStyleSheet(sheet); // Adds the stuff in the StyleSheet to the referenced tags.
     //                                                                   Set this manually.  V V V V
-    $("#percent").html("Our 2015 raffle has ended! | Tickets sold: " + deliberateSold + "% | $11,250+ earned<div class='raffle-link'> | <a href='/raffle'>Winners >></a></div>");
+    $("#percent").html("Our 2015 raffle has ended! | Tickets sold: " + deliberateSold + "% | $11,250+ earned<span class='raffle-link'> | <a href='/raffle'>Winners >></a></span>");
 
     // This changes the tooltip to not include the tickets checked out.
     if($("#raffle-meter-container").length > 0) document.getElementById("raffle-tooltip").setAttribute("data-content", "Blue: Tickets sold \nYellow: Tickets not sold");
